6eec2dfde820d8a1c50bd468d326225f38108e06
Mason Protter <mason.protter@gmail.com>
2018-08-20 17:24:18 -0600

    fixed test

--- test/testscript.jl
+++ test/testscript.jl
@@ -1,42 +1,66 @@
-using Test
+using Test, Unicode
+Base.include(@__MODULE__, joinpath(Sys.BINDIR, "..", "share", "julia", "test", "testhelpers", "FakePTYs.jl"))
+import .FakePTYs: open_fake_pty
 
+slave, master = open_fake_pty()
+
+CTRL_C = '\x03'
+
+# Script that we want the REPL to execute, here simply st for Pkg REPLMode
+test_script = """
 using ReplMaker
 
 function parse_to_expr(s)
-    quote Meta.parse($s) end
+    quote Meta.parse(\$s) end
 end
 
-function __init__(_)
-    try
-        @async begin
-            while !isdefined(Base, :active_repl)
-                sleep(0.05)
-            end
-            sleep(1) # for extra safety
-            initrepl(parse_to_expr, 
-                     prompt_text="Expr> ",
-                     prompt_color = :blue, 
-                     start_key=')', 
-                     mode_name="Expr_mode")
+initrepl(parse_to_expr, 
+         prompt_text="Expr> ",
+         prompt_color = :blue, 
+         start_key=')', 
+         mode_name="Expr_mode")
 
-            @test Base.active_repl.interface.modes[end].prompt == "Expr> "
-            @test Base.active_repl.interface.modes[end].sticky == true
-            @test 1 == 2
-            exit()
+) x + 1
+
+"""*CTRL_C
+
+
+function run_test()
+    slave, master = open_fake_pty()
+    # Start a julia process
+    p = run(`$(Base.julia_cmd()) --history-file=no --startup-file=no`, slave, slave, slave; wait=false)
+    
+    # Read until the prompt
+    readuntil(master, "julia>", keep=true)
+    done = false
+    repl_output_buffer = IOBuffer()
+
+    # A task that just keeps reading the output
+    @async begin
+        while true
+            done && break
+            write(repl_output_buffer, readavailable(master))
         end
-    catch e
-        println(e)
-        exit()
     end
+
+    # Execute our "script"
+    for l in split(test_script, '\n'; keepempty=false)
+        write(master, l, '\n')
+    end
+
+    # Let the REPL exit
+    write(master, "exit()\n")
+    wait(p)
+    done = true
+
+    # Gather the output
+    repl_output = String(take!(repl_output_buffer))
+    println(repl_output)
+    return split(repl_output, '\n'; keepempty=false)
 end
 
-atreplinit(__init__)
+
+out = run_test();
 
 
-# using Test
-
-# atreplinit((_) -> begin
-#            @test 1 == 2
-#            exit()
-#            end
-#            )
+@test out[end-5] == "\e[?2004h\r\e[0K\e[34m\e[1mExpr> \e[0m\e[0m\r\e[6C\r\e[6C\r\e[0K\e[34m\e[1mExpr> \e[0m\e[0m\r\e[6C\r\e[6C^C\r"
